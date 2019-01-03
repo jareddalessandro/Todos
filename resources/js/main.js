f82b21beca92b77fa4c2c2f337d4539a6e7faf1d
Dalessandro, Jared <jdalessandro@tep.com>
2019-01-03 15:18:52 -0700

    Code Cleanups

--- resources/js/main.js
+++ resources/js/main.js
@@ -4,6 +4,16 @@ var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem(
     completed: []
 };
 
+// Color constants
+var darkBackground = '#272829'; // dark grey
+var darkCompletedItem = '#63b26b'; // deep green
+var darkItem = '#4b4e51'; // less dark grey
+var darkModeText = '#fff'; // white
+var lightBackground = '#edf0f1'; // light grey
+var lightCompletedItem = '#d5ffdc'; // light green
+var lightItem = '#fff'; // white
+var lightModeText = '#444'; // darkgrey
+
 // Remove and complete icons in SVG format
 var removeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6V18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
 var completeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>';
@@ -15,8 +25,7 @@ updateColors();
 
 // Mode Toggle Switch
 document.getElementById('checkbox').addEventListener('change', function () {
-    var checkBox = document.getElementById('checkbox');
-    if (checkBox.checked) {
+    if (this.checked) {
         localStorage.setItem('modeSetting', 'dark')
     }
     else {
@@ -173,8 +182,6 @@ function addItemToDOM(text, completed) {
         if (this.style.backgroundColor !== '#9b26aa') {
             this.style.backgroundColor = '#9b26aa';
             this.style.color = '#fff';
-            console.log('In the if');
-
         }
     });
 
@@ -184,15 +191,6 @@ function addItemToDOM(text, completed) {
 // Sets body and list colors for dark mode and light mode
 // Animations and hover effects are in style.css
 function updateColors() {
-    var darkBackground = '#272829'; // dark grey
-    var darkCompletedItem = '#63b26b'; // deep green
-    var darkItem = '#4b4e51'; // less dark grey
-    var darkModeText = '#fff'; // white
-
-    var lightBackground = '#edf0f1'; // light grey
-    var lightCompletedItem = '#d5ffdc'; // light green
-    var lightItem = '#fff'; // white
-    var lightModeText = '#444'; // darkgrey
 
     // Dark Mode
     if (localStorage.getItem('modeSetting') == 'dark') {
